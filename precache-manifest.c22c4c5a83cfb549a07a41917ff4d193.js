7e1941733c5cef0293bfb3787a2280ab1b27fd3e
Paloma Ania <paloma.aniabenito@gmail.com>
2019-01-31 19:44:06 +0100

    Updates

---
+++ precache-manifest.c22c4c5a83cfb549a07a41917ff4d193.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "545f5843a5f2a8a671d2",
+    "url": "/string-projects/static/js/runtime~main.545f5843.js"
+  },
+  {
+    "revision": "bdba1ab3fb0fe765a748",
+    "url": "/string-projects/static/js/main.bdba1ab3.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/string-projects/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "bdba1ab3fb0fe765a748",
+    "url": "/string-projects/static/css/main.1f7b5e4a.chunk.css"
+  },
+  {
+    "revision": "4c3312bd95c06b1fe196292fe2d1eecb",
+    "url": "/string-projects/index.html"
+  }
+];
