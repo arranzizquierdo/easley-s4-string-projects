543625ca9f27480814c67879640dafea5bab361b
Paloma Ania <paloma.aniabenito@gmail.com>
2019-02-01 13:35:46 +0100

    Change some name classes in SendMessage component

--- src/components/SendMessage/index.js
+++ src/components/SendMessage/index.js
@@ -5,14 +5,14 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 class SendMessage extends Component {
   render() {
     return (
-      <div className="sendmessage_container">
+      <div className="sendmessage__container">
         <label htmlFor="input" />
         <input
           id="input"
           className="sendmessage__input"
           onChange={this.props.inputSendMessage}
         />
-        <button className="sendmessage_button">
+        <button className="sendmessage__button">
           <FontAwesomeIcon icon="paper-plane" className="paperplane__styles" />
         </button>
       </div>
