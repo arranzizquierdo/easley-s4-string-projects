5ef7f137e1ee48c547f7ef595a2c66996285430b
laurasanra <laura.san.rada@gmail.com>
2019-02-01 20:16:06 +0100

    create error component

---
+++ src/components/ErrorMessage/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './ErrorMessage.scss';
+
+class ErrorMessage extends Component{
+    render(){
+        return "hola"
+    }
+}
+
+export default ErrorMessage;
