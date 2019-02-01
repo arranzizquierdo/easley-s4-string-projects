d44227a1feb2b78c1e5c8dba91b6b6770d56324a
laurasanra <laura.san.rada@gmail.com>
2019-02-01 17:56:15 +0100

    add form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import './stylesheets/App.scss';
 import LogIn from './components/LogIn';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faEyeSlash, faKey } from '@fortawesome/free-solid-svg-icons';
+library.add(faEyeSlash, faKey);
+
 
 class App extends Component {
   render() {
--- src/components/LogIn/index.js
+++ src/components/LogIn/index.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './LogIn.scss';
 
 class LogIn extends Component {
     render(){
-        return "hola"
+        return <div>
+            <h1>Inicia sesión</h1>
+            <form action="/signup" method="post">
+            <label htmlFor="user-name"/>
+            <input type="text" id="user-name" placeholder="Tu dirección de email o nombre de usuario" name="user-name"/>
+            <label htmlFor="password"/>
+            <input type="text" id="password" placeholder="Contraseña" name="password"/>
+            <FontAwesomeIcon
+            icon="faEyeSlash" />
+            </form>
+           
+        </div>
     }
 }
 
