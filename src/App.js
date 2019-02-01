978de6755614bcba92f236a022268c689ec11119
aroatdlt <aroatato@gmx.com>
2019-02-01 17:58:20 +0100

    Done each page in components and router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
 import React, { Component } from 'react';
 import './stylesheets/App.scss';
+import { Route, Switch } from 'react-router-dom';
+import LandingPage from './components/LandingPage/LandingPage';
+import MainPage from './components/MainPage/MainPage';
+import ConversationPage from './components/ConversationPage/ConversationPage';
+import ConversationThreading from './components/ConversationThreading/ConversationThreading';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+
+    }
+  }
   render() {
     return (
-      "WE in process..."
-    );
+      <Switch>
+        <Route 
+          exact path="/"  
+          render={props => (
+            <LandingPage/>
+          )}
+        />
+        <Route 
+          path="/mainpage"
+          render={props => (
+            <MainPage/>
+          )}
+        />
+        <Route 
+          path="/conversationpage"
+          render={props => (
+            <ConversationPage/>
+          )}
+        />
+        <Route 
+          path="/conversationthreading"
+          render={props => (
+            <ConversationThreading/>
+          )}
+        />
+      </Switch>
+
+    )
   }
 }
 
