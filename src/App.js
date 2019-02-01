515ee9b01b2ee7a6b6a46c7dfe6e9b86526644d2
arranzizquierdo <laura.arranz88@gmail.com>
2019-02-01 19:15:04 +0100

    Responsive style.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import './stylesheets/App.scss';
 import Header from './components/Header';
+import IndividualMessage from './components/IndividualMessage';
 
 class App extends Component {
   render() {
     return (
       // "WE in process..."
+      <Fragment>
       <Header />
+      <IndividualMessage />
+      </Fragment>
     );
   }
 }
