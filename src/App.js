a88d86d90921a02ab6f12eaabb91f826e3a598da
arranzizquierdo <laura.arranz88@gmail.com>
2019-02-01 17:37:08 +0100

    Create component Header js and scss

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './stylesheets/App.scss';
+import Header from './components/Header';
 
 class App extends Component {
   render() {
     return (
-      "WE in process..."
+      // "WE in process..."
+      <Header />
     );
   }
 }
