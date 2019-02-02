6875f77de1b5b81a3bb5b433750c7b62bd66d65b
arranzizquierdo <laura.arranz88@gmail.com>
2019-02-02 13:51:01 +0100

    Header conversationpage style responsive

--- src/components/ConversationPage/index.js
+++ src/components/ConversationPage/index.js
@@ -2,11 +2,20 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './ConversationPage.scss';
 import Header from '../Header';
+import groupImage from '../../images/groupImage.jpg';
 
 class ConversationPage extends Component {
     render() { 
         return  (
-            <Header />
+            <Header>
+                <div className = "group__container">
+                    <img className = "group__image" src={groupImage} alt="Icono grupo"/>
+                    <span className = "container__text">
+                    <p className = "group__title">Recetas y menús</p>
+                    <p className = "group__persons">56 personas</p>
+                    </span>
+                </div>
+            </Header>
         ) 
     }
 }
