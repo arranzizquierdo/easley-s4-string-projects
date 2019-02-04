454166e5777fc5c115297f3013ad1dce88ccef10
aroatdlt <aroatato@gmx.com>
2019-02-04 09:32:40 +0100

    Remove margin and check modal

--- src/components/MainPage/index.js
+++ src/components/MainPage/index.js
@@ -2,11 +2,17 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './MainPage.scss';
 import Modal from '../Modal';
+import image from '../../images/temporal_5.jpg';
 
 class MainPage extends Component {
-    render() { 
+    render() {
         return (
-            <Modal/>
+            <React.Fragment>
+                <img className="image" src={image}
+                alt="Tom Hanks"
+                />
+                <Modal />
+            </React.Fragment>
         )
     }
 }
