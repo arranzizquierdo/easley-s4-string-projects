1873fe186f9cc2b3096f49dabf61113cb5fac4f6
aroatdlt <aroatato@gmx.com>
2019-02-05 17:40:52 +0100

    Fix main page styles

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import MainPage from './components/MainPage';
 import ConversationPage from './components/ConversationPage';
 import ConversationThreading from './components/ConversationThreading';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { fetchToken } from "./components/services/TokenService";
 import {
   faEllipsisH,
   faEyeSlash,
