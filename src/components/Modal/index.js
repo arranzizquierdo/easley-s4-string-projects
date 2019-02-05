ac6e55d5b7a2f577d48664d9ad9f9fe5528bd707
arranzizquierdo <laura.arranz88@gmail.com>
2019-02-05 19:50:09 +0100

    Create Link to landing page in modal

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./Modal.scss";
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class Modal extends Component {
   render() {
@@ -13,7 +14,11 @@ class Modal extends Component {
       <div className={`modal__container ${hidden}`}>
         <ul className="modal__list">
           <li className="modal__item-title">¿Qué quieres hacer?</li>
-          <li className="modal__item modal__item-sesion">Cerrar Sesión</li>
+          <li className="modal__item modal__item-sesion">
+            <Link className="style_link" to="/">
+            Cerrar Sesión
+            </Link>
+          </li>
           <li className="modal__item modal__item-cancel" onClick={cancelClickModal}>Cancelar</li>
         </ul>
       </div>
