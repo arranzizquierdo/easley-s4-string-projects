753a0b98522ce51edf9d77cbe9671fa696f5b956
rdfornieles <rdfornieles@gmail.com>
2019-02-11 18:10:50 +0100

    fix conversation page

--- src/components/ConversationThreading/index.js
+++ src/components/ConversationThreading/index.js
@@ -23,7 +23,7 @@ class ConversationThreading extends Component {
           </div>
         </Header>
         <main className="main__conversationThreading">
-          <Link className="style_link" to="/conversation-page">
+          <Link className="style_link" to="/conversation-page/:id">
             <GoBack />
           </Link>
           <IndividualMessage isLoading={isLoading} dataUser={dataUser}/>
