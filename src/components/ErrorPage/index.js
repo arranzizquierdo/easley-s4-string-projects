5f1e6e5eaf427efdf21226950dc34abebede7d31
rdfornieles <rdfornieles@gmail.com>
2019-02-18 17:55:27 +0100

    done error page style

--- src/components/ErrorPage/index.js
+++ src/components/ErrorPage/index.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import './ErrorPage.scss';
 
 class ErrorPage extends Component{
 render(){
   return <div className="errorPage__container">
-  <p>Ups, algo salió mal...</p>
-  <p>Inténtalo de nuevo más tarde</p>
+  <p className = "errorPage__text">Ups, algo salió mal...</p>
+  <p className = "errorPage__text">Inténtalo de nuevo más tarde.</p>
   </div>
 }
 }
